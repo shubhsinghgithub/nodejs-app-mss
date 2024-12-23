0198feb9f559d71575eb0132d91a0be5156d243a
shubhsinghgithub <91608068+shubhsinghgithub@users.noreply.github.com>
2024-12-23 15:33:31 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://81.17.22.74/:9000/',
+  serverUrl: 'http://81.17.22.74/:9000',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
