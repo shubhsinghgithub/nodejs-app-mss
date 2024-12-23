d335c3bffd975b4c88dd5ba796302d4630740f9a
shubhsinghgithub <91608068+shubhsinghgithub@users.noreply.github.com>
2024-12-23 14:51:16 +0530

    Update sonar-project.js
    sonar details

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.111.23.2:9000/',
+  serverUrl: 'http://81.17.22.74/:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'squ_8b5bf1e9057b32ea49a542206eadec00089c43cf',
+	    'sonar.login': 'squ_3a7a6bfa816374edbd6d9b558896437c98a2a3d7',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
